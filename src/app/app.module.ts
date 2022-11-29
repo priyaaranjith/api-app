7438dd9bf78dc39dd26eedd7988ed5a791166cbc
priyaaranjith <ranjithpriya307@gmail.com>
2022-11-29 10:36:06 +0530

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
+import { USDataComponent } from './u-sdata/u-sdata.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PassengerDataComponent
+    PassengerDataComponent,
+    USDataComponent
   ],
   imports: [
     BrowserModule,
